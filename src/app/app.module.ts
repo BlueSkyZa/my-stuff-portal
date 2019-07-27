f8607696f174098212dd9b1cba4012d2f7c0192b
BlueSkyUk <chris.nash@da-systems.co.uk>
2019-07-27 13:48:55 +0100

    basic shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,86 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+import { MaterialModule } from './material.module';
+import { ToastrModule } from 'ngx-toastr';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { RoutingModule } from './routing.module';
+
+import { AuthActivator } from './activator/auth.activator';
+import { AuthInterceptor } from './interceptor/auth.interceptor';
+
+import { AuthService } from './service/auth.service';
+import { BusyService } from './service/busy.service';
+import { HttpService } from './service/http.service';
+import { IdentityService } from './service/identity.service';
+import { NotifierService } from './service/notifier.service';
+import { StuffService } from './service/stuff.service';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { StuffComponent } from './component/stuff/stuff.component';
+import { UserComponent } from './component/user/user.component';
+import { BusyComponent } from './component/busy/busy.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    StuffComponent,
+    UserComponent,
+    BusyComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      autoDismiss: false,
+      preventDuplicates: true,
+      resetTimeoutOnDuplicate: true,
+      positionClass: 'toast-bottom-right',
+    }),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RoutingModule
   ],
-  providers: [],
+  providers: [
+    HttpClient,
+    AuthActivator,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    AuthService,
+    BusyService,
+    HttpService,
+    IdentityService,
+    NotifierService,
+    StuffService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
